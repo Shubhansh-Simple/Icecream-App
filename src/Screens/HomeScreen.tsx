7c02f5bf9e06b2fa04cdbdc98f83ef432fabf15e
Shubhansh Programmer <shubhansh7777@gmail.com>
2021-06-21 17:53:19 +0530

    Starts Building Homepage of the application.

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React,{useEffect}  from 'react';
+import React, { useState,useEffect }  from 'react';
 import { createIcecream, 
          createStock } from '../Database/StarterFunction';
 
@@ -6,7 +6,38 @@ import { View,
          Text,
          StyleSheet } from 'react-native';
 
+import IcecreamInput from '../Components/CurrentStock/IcecreamInput';
+
+// DATABASE
+import queryExecutor    from '../Database/StarterFunction';
+import {stock}          from '../Database/Queries';
+
+// LOCAL
+import commonStyle from '../Styles/commonStyle';
+import Icon        from '../Components/Buttons/Icon';
+import {extractId}      from '../CleanCode/CleanFunction';
+
 export default function HomeScreen() {
+
+  const [ currentStockList,    setCurrentStockList] = useState([])
+  const [ icecreamInput, setIcecreamInput ]         = useState(false)
+
+  function readStock(){
+    /*
+     *READ STOCK
+     */
+    queryExecutor( stock.readStockQuery,
+                   null,
+                   'Stock-R',
+                   databaseData=>setCurrentStockList(databaseData)
+                 )
+  }
+
+  function insertSale(){
+
+  }
+
+
 
   useEffect( ()=>{
     createIcecream()
@@ -15,8 +46,30 @@ export default function HomeScreen() {
 
   return(
     <View style={styles.container}>
-      <Text>Welcome to HomePage</Text>
-    </View>
+
+      {/* ICECREAM INPUT MODAL */}
+      <IcecreamInput
+        title            ='Starts Solding Icecream'
+        description      ='Choose icecream & quantities to sold'
+        visible          ={icecreamInput}
+        setVisible       ={ (bool:boolean)=>setIcecreamInput(bool) }
+        currentIcecreamId={ extractId(currentStockList) }          //calling func.
+
+        submitData ={ (item,quantity,bool)=>insertSale()}
+      />
+
+      {/* ICECREAM INPUT MODAL CALLER */}
+      <View style={ commonStyle.positionBtnContainer}>
+        <Icon 
+          iconName ='add-shopping-cart'
+          iconSize ={50}
+          color    ='white'
+          bgCircleColor='#fc035e'
+          bottomTitle={false}
+          callBack ={ ()=>setIcecreamInput(true) }
+        />
+      </View>
+   </View>
   )
 }
 
